bdb664fe78539da6c2d5544703b7e76d8f41128f
SERGIO DAVID CRUZ ANDUQUIA <sergiocruza14@gmail.com>
2026-06-15 14:36:51 -0500

    Update weapon.py

--- weapon.py
+++ weapon.py
@@ -1,5 +1,6 @@
 import pygame
 import constantes
+import math
 
 class Weapon():
     def __init__(self, image):
@@ -10,9 +11,38 @@ class Weapon():
 
     def update(self, Player):
         self.shape.center = Player.shape.center
-        self.shape.x = self.shape.x + Player.shape.width /2
-        self.shape.y = self.shape.y + Player.shape.height /6.5
+        if Player.flip == False:
+            self.shape.x = self.shape.x + Player.shape.width /1.5
+            self.shape.y = self.shape.y + Player.shape.height /6.5
+            self.rotate_weapon(False)
+
+        if Player.flip == True:
+            self.shape.x = self.shape.x - Player.shape.width /1.5
+            self.shape.y = self.shape.y + Player.shape.height /6.5
+            self.rotate_weapon(True)
+        
+        #mover la pistola con el mouse
+        mouse_pos = pygame.mouse.get_pos()
+        distance_x = mouse_pos[0] - self.shape.centerx
+        distance_y = -(mouse_pos[1] - self.shape.centery)
+        self.angle = math.degrees(math.atan2(distance_y, distance_x))
+
+        print(self.angle)
+        
+    def rotate_weapon(self, rotate):
+        if rotate == True:
+            image_flip = pygame.transform.flip(self.image_original,
+                                               True, False)
+            self.image = pygame.transform.rotate(image_flip, self.angle)
+        else:
+            image_flip = pygame.transform.flip(self.image_original,
+                                               False, False)
+            self.image = pygame.transform.rotate(image_flip, self.angle)
+            
 
     def draw(self, interface):
+        self.image = pygame.transform.rotate(self.image, self.angle)
         interface.blit(self.image, self.shape)
-        pygame.draw.rect(interface, constantes.COLOR_GUN, self.shape, 1)
+        #pygame.draw.rect(interface, constantes.COLOR_GUN, self.shape, 1)
+    
+    
