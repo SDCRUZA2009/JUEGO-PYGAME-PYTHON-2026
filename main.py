be88daf144870937619d081a19d674bc0a2e17a0
SERGIO DAVID CRUZ ANDUQUIA <sergiocruza14@gmail.com>
2026-06-15 14:37:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import constantes
 from personaje import Player
+from weapon import Weapon 
 
 pygame.init()
 
@@ -14,14 +15,23 @@ def escalar_img(image, scale):
     nueva_imagen = pygame.transform.scale(image, (w*scale, h*scale))
     return nueva_imagen
 
+#importar imagenes
+#Personaje
 animaciones = []
-for i in range (10):
-    img = player_image = pygame.image.load(f"assets//images//characters//player//PLayer_{i}.png")
+for i in range (4):
+    img = player_image = pygame.image.load(f"assets//images//characters//player//Player_{i}.png").convert_alpha()
     img = escalar_img(img, constantes.SCALA_PLAYER)
     animaciones.append(img)
 
+#arma
+image_gun = pygame.image.load(f"assets//images//weapons//gun.png").convert_alpha()
+image_gun = escalar_img(image_gun, constantes.SCALA_GUN)
+
+#crear un jugador de la clase personaje
 player = Player(40, 40, animaciones)
 
+#crear un arma de la clase weapon
+gun = Weapon(image_gun)
 
 #definir las variables de movimiento del jugador
 move_up = False
@@ -37,7 +47,6 @@ while run:
     
     #QUE VAYA A 60 FPS
     clock.tick(constantes.FPS)
-
     window.fill(constantes.COLOR_BG)
 
     #calcular el movimiento del jugador
@@ -56,9 +65,19 @@ while run:
     #mover al jugador
     player.move(delta_x, delta_y)
 
+    #actualizar el estado del jugador
     player.update()
 
+    #actualizar el estado del arma
+    gun.update(player)
+
+    #dibujar el jugador
     player.draw(window)
+
+    #dibujar el arma
+    gun.draw(window)
+
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
