c9d39d9ebf4221923f2ae3c1c94a6d272e088991
SERGIO DAVID CRUZ ANDUQUIA <sergiocruza14@gmail.com>
2026-06-26 02:05:00 -0500

    Enhance Player class with energy and live tracking
    Added energy and live attributes to Player class.

--- personaje.py
+++ personaje.py
@@ -2,7 +2,9 @@ import pygame
 import constantes
 
 class Player():
-    def __init__(self, x, y, animaciones):
+    def __init__(self, x, y, animaciones, energy):
+        self.energy = energy
+        self.live = True
         self.flip = False
         self.animaciones = animaciones
         #imagen de las animacion que se esta mostrando actualmente
@@ -10,9 +12,9 @@ class Player():
         #aqui se almacena la hora actual( en milisegundos desde que se inicio "pygame")
         self.update_time = pygame.time.get_ticks()
         self.image = animaciones[self.frame_index]
-        self.shape = pygame.Rect(0, 0, constantes.WIDTH_PLAYER,
-                                  constantes.HEIGHT_PLAYER)
-        self.shape.center = (x, y)
+        self.shape = self.image.get_rect()
+        self.shape.x = x
+        self.shape.y = y
     
     def move(self, delta_x, delta_y):
         if delta_x < 0:
@@ -24,6 +26,11 @@ class Player():
         self.shape.y = self.shape.y + delta_y
     
     def update(self):
+        #comprobar si el personaj eha muerto
+        if self.energy <= 0:
+            self.energy = 0
+            self.live = False
+            
         cooldown_animacion = 100
         self.image = self.animaciones[self.frame_index]
         if pygame.time.get_ticks() - self.update_time >= cooldown_animacion:
